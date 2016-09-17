cc9655d06cde40453c53d32f65067f92d1455449
Lab103 <Lab103@LAB101-14>
2016-09-17 11:08:17 -0300

    Inclusão do caso de teste cadastrar empresa

--- sceweb/src/br/sceweb/servico/FabricaDeConexoes.java
+++ sceweb/src/br/sceweb/servico/FabricaDeConexoes.java
@@ -8,5 +8,15 @@ import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
 
 public class FabricaDeConexoes {
+	public Connection getConnection(){
+		String url = "jdbc:mysql://localhost/sceweb";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return (Connection) DriverManager.getConnection(url,"root","alunofatec");
+		}
+		catch (Exception e){
+			throw new RuntimeException(e);
+		}
+	}
 	
 }
--- sceweb/src/br/sceweb/teste/UC01CadastrarEmpresa1.java
+++ sceweb/src/br/sceweb/teste/UC01CadastrarEmpresa1.java
@@ -3,22 +3,59 @@ package br.sceweb.teste;
 import static org.junit.Assert.*;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class UC01CadastrarEmpresa1 {
+import br.sceweb.modelo.Empresa;
+import br.sceweb.modelo.EmpresaDAO;
 
+public class UC01CadastrarEmpresa1 {
+	static Empresa empresa;
+	static EmpresaDAO empresaDAO;
+	
+	/**
+	 * ESTABELECE AS PRECONDIÇÕES DE TESTE
+	 * @throws Exception
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		empresaDAO = new EmpresaDAO();
+		empresa = new Empresa();
+		empresa.setNomeDaEmpresa("empresa x");
+		empresa.setCnpj("89424232000180");
+		empresa.setNomeFantasia("empresa x");
+		empresa.setEndereco("rua taquari");
+		empresa.setTelefone("2222");
+	}
+	
+	@Before
+	public void exluiCNPJ(){
+		empresaDAO.exclui("89424232000180");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		
 	}
 
+	/**
+	 * Verificar o comportamento do sistema na inclusão de uma empresa 
+	 * com sucesso
+	 * */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void CT01UC01CadastrarEmpresa_com_sucesso() {
+		assertEquals(1,empresaDAO.adiciona(empresa));
+	}
+	
+	/**
+	 * Verificar o comportamento do sistema na inclusão de uma empresa 
+	 * com CNPJ inválido
+	 * */
+	@Test(expected=RuntimeException.class)
+	public void CT02UC01CadastrarEmpresa_com_CNPJ_invalido() {
+		empresaDAO.adiciona(empresa);
+		empresaDAO.adiciona(empresa);
 	}
 
 }
